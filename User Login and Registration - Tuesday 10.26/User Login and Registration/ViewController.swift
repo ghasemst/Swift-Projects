e6e4844dc4055178b94254921d64cd643e73ba2a
Ghasem Tabatabaei <ghasem@tabatabaei.com>
2015-06-23 15:37:04 +0200

    2 Projects has been added

---
+++ User Login and Registration - Tuesday 10.26/User Login and Registration/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  User Login and Registration
+//
+//  Created by Ghasem Tabatabaei on 22/06/15.
+//  Copyright © 2015 Ghasem Tabatabaei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
